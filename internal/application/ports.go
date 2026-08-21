a6b06da0c76c94257b0df5ca8e325703a078a311
Sayyar Ahmad <m.sayyarahmad1995@gmail.com>
2026-08-21 23:57:54 +0500

    feat: add auth provisioning ports

--- internal/application/ports.go
+++ internal/application/ports.go
@@ -25,6 +25,11 @@ var (
 type UserRepository interface {
 	Get(ctx context.Context, id user.ID) (user.User, error)
 	GetByOIDCSubject(ctx context.Context, subject string) (user.User, error)
+	ProvisionByOIDCSubject(ctx context.Context, subject string) (user.User, error)
+}
+
+type SessionResolver interface {
+	ResolveSubject(ctx context.Context, token string) (string, error)
 }
 
 type RideRepository interface {
